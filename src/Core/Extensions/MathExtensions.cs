9f0b6797ee3164af09740edb2c83afa485105553
JoshuaLight <ashkariel.eter@gmail.com>
2018-06-12 15:04:10 -0700

    Added `Abs` extension-methods that are replacement for `Math.Abs`.

---
+++ src/Core/Extensions/MathExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pocket.Common
+{
+    public static class DoubleExtensions
+    {
+        public static short Abs(this short self) => Math.Abs(self);
+        public static int Abs(this int self) => Math.Abs(self);
+        public static long Abs(this long self) => Math.Abs(self);
+        public static float Abs(this float self) => Math.Abs(self);
+        public static double Abs(this double self) => Math.Abs(self);
+        public static decimal Abs(this decimal self) => Math.Abs(self);
+    }
+}
