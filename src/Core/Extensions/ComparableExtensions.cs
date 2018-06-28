dff0f5e0155efb4c8f4dd43dfb78cb308d980103
JoshuaLight <ashkariel.eter@gmail.com>
2018-06-28 07:32:29 -0700

    Added comments to `ComparableExtensions`.

--- src/Core/Extensions/ComparableExtensions.cs
+++ src/Core/Extensions/ComparableExtensions.cs
@@ -2,17 +2,48 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents extension-methods for <see cref="IComparable{T}"/>.
+    /// </summary>
     public static class ComparableExtensions
     {
+        /// <summary>
+        ///     Checks whether <code>self</code> is less than specified value.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Value to compare.</param>
+        /// <typeparam name="T">Type of comparing values.</typeparam>
+        /// <returns><code>true</code>, if <code>this</code> is less than <paramref name="other"/>, otherwise <code>false</code>.</returns>
         public static bool IsLess<T>(this T self, T other) where T : IComparable<T> =>
             self.CompareTo(other) == -1;
         
+        /// <summary>
+        ///     Checks whether <code>self</code> is less than or equal to specified value.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Value to compare.</param>
+        /// <typeparam name="T">Type of comparing values.</typeparam>
+        /// <returns><code>true</code>, if <code>this</code> is less than or equal to <paramref name="other"/>, otherwise <code>false</code>.</returns>
         public static bool IsLessOrEqual<T>(this T self, T other) where T : IComparable<T> =>
             self.CompareTo(other) <= 0;
         
+        /// <summary>
+        ///     Checks whether <code>self</code> is greater than specified value.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Value to compare.</param>
+        /// <typeparam name="T">Type of comparing values.</typeparam>
+        /// <returns><code>true</code>, if <code>this</code> is greater than <paramref name="other"/>, otherwise <code>false</code>.</returns>
         public static bool IsGreater<T>(this T self, T other) where T : IComparable<T> =>
             self.CompareTo(other) == 1;
         
+        /// <summary>
+        ///     Checks whether <code>self</code> is greater than or equal to specified value.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Value to compare.</param>
+        /// <typeparam name="T">Type of comparing values.</typeparam>
+        /// <returns><code>true</code>, if <code>this</code> is greater than or equal to <paramref name="other"/>, otherwise <code>false</code>.</returns>
         public static bool IsGreaterOrEqual<T>(this T self, T other) where T : IComparable<T> =>
             self.CompareTo(other) >= 0;
     }
