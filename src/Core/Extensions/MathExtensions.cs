ffb7bd91ee17c4bda42064b0e67ad669e39d2822
JoshuaLight <ashkariel.eter@gmail.com>
2018-06-28 07:36:40 -0700

    Added comments to `MathExtensions`.

--- src/Core/Extensions/MathExtensions.cs
+++ src/Core/Extensions/MathExtensions.cs
@@ -2,13 +2,57 @@
 
 namespace Pocket.Common
 {
-    public static class DoubleExtensions
+    /// <summary>
+    ///     Represents extension-methods for number-types.
+    /// </summary>
+    public static class MathExtensions
     {
+        /// <summary>
+        ///     Returns the absolute value of <code>self</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <exception cref="OverflowException"><paramref name="self"/> equals <code>MinValue</code>.</exception>
+        /// <returns>Absolute value of <paramref name="self"/>.</returns>
         public static short Abs(this short self) => Math.Abs(self);
+        
+        /// <summary>
+        ///     Returns the absolute value of <code>self</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <exception cref="OverflowException"><paramref name="self"/> equals <code>MinValue</code>.</exception>
+        /// <returns>Absolute value of <paramref name="self"/>.</returns>
         public static int Abs(this int self) => Math.Abs(self);
+        
+        /// <summary>
+        ///     Returns the absolute value of <code>self</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <exception cref="OverflowException"><paramref name="self"/> equals <code>MinValue</code>.</exception>
+        /// <returns>Absolute value of <paramref name="self"/>.</returns>
         public static long Abs(this long self) => Math.Abs(self);
+        
+        /// <summary>
+        ///     Returns the absolute value of <code>self</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <exception cref="OverflowException"><paramref name="self"/> equals <code>MinValue</code>.</exception>
+        /// <returns>Absolute value of <paramref name="self"/>.</returns>
         public static float Abs(this float self) => Math.Abs(self);
+        
+        /// <summary>
+        ///     Returns the absolute value of <code>self</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <exception cref="OverflowException"><paramref name="self"/> equals <code>MinValue</code>.</exception>
+        /// <returns>Absolute value of <paramref name="self"/>.</returns>
         public static double Abs(this double self) => Math.Abs(self);
+        
+        /// <summary>
+        ///     Returns the absolute value of <code>self</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <exception cref="OverflowException"><paramref name="self"/> equals <code>MinValue</code>.</exception>
+        /// <returns>Absolute value of <paramref name="self"/>.</returns>
         public static decimal Abs(this decimal self) => Math.Abs(self);
     }
 }
