7cc06c7a2899e825378c039e0e6a0e5a14ee2bc8
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-01 09:49:25 -0700

    Added comments to `TaskExtensions`.

--- src/Core/Extensions/TaskExtensions.cs
+++ src/Core/Extensions/TaskExtensions.cs
@@ -2,11 +2,22 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents extension-methods for <see cref="Task{TResult}"/>.
+    /// </summary>
     public static class TaskExtensions
     {
+        /// <summary>
+        ///     Waits until <code>self</code> task is completed or for specified amount of time.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="timeoutMs">Amount of time to wait.</param>
+        /// <typeparam name="T">Type of task result.</typeparam>
+        /// <returns><code>self.Result</code> if task is completed within <paramref name="timeoutMs"/>, otherwise <code>null</code>.</returns>
         public static async Task<T> WithTimeout<T>(this Task<T> self, int timeoutMs)
         {
             var timeout = Task.Delay(timeoutMs);
+            
             await Task.WhenAny(self, timeout);
 
             return timeout.IsCompleted && !self.IsCompleted
@@ -14,23 +25,38 @@ namespace Pocket.Common
                 : self.Result;
         }
         
+        /// <summary>
+        ///     Waits until <code>self</code> task with <see cref="Result"/> is completed or for specified amount of time.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="timeoutMs">Amount of time to wait.</param>
+        /// <returns><code>self.Result</code> if task is completed within <paramref name="timeoutMs"/>, otherwise <code>null</code>.</returns>
         public static async Task<Result> WithTimeout(this Task<Result> self, int timeoutMs)
         {
             var timeout = Task.Delay(timeoutMs);
+            
             await Task.WhenAny(self, timeout);
 
             return timeout.IsCompleted && !self.IsCompleted
-                ? Result.Failed("Failed by timeout: " + timeoutMs + ".")
+                ? Result.Failed($"Failed by timeout: {timeoutMs}.")
                 : self.Result;
         }
         
+        /// <summary>
+        ///     Waits until <code>self</code> task with <see cref="Result{T}"/> is completed or for specified amount of time.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="timeoutMs">Amount of time to wait.</param>
+        /// <typeparam name="T">Type of task result.</typeparam>
+        /// <returns><code>self.Result</code> if task is completed within <paramref name="timeoutMs"/>, otherwise <code>null</code>.</returns>
         public static async Task<Result<T>> WithTimeout<T>(this Task<Result<T>> self, int timeoutMs)
         {
             var timeout = Task.Delay(timeoutMs);
+            
             await Task.WhenAny(self, timeout);
 
             return timeout.IsCompleted && !self.IsCompleted
-                ? Result.Failed<T>("Failed by timeout: " + timeoutMs + ".")
+                ? Result.Failed<T>($"Failed by timeout: {timeoutMs}.")
                 : self.Result;
         }
     }
