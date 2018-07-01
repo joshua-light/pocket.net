a7b5fc0a212b6656f0de4c1ae550185bb1ecda97
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-01 04:11:03 -0700

    Added tests for `MathExtensions.Abs` method.

--- src/Tests/Extensions/MathExtensionsTest.cs
+++ src/Tests/Extensions/MathExtensionsTest.cs
@@ -1,9 +1,63 @@
-﻿using Xunit;
+﻿using System;
+using Xunit;
 
 namespace Pocket.Common.Tests.Extensions
 {
   public class MathExtensionsTest
   {
+    #region Abs
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(-1)]
+    [InlineData(short.MaxValue)]
+    [InlineData(short.MinValue + 1)]
+    public void Abs_ShouldBehaveAsMathAbs_IfShort(short value) => Assert.Equal(Math.Abs(value), value.Abs());
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(-1)]
+    [InlineData(int.MaxValue)]
+    [InlineData(int.MinValue + 1)]
+    public void Abs_ShouldBehaveAsMathAbs_IfInt(int value) => Assert.Equal(Math.Abs(value), value.Abs());
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(-1)]
+    [InlineData(long.MaxValue)]
+    [InlineData(long.MinValue + 1)]
+    public void Abs_ShouldBehaveAsMathAbs_IfLong(long value) => Assert.Equal(Math.Abs(value), value.Abs());
+    
+    [Theory]
+    [InlineData(0f)]
+    [InlineData(1.1f)]
+    [InlineData(-1.1f)]
+    [InlineData(float.MaxValue)]
+    [InlineData(float.MinValue + 1)]
+    public void Abs_ShouldBehaveAsMathAbs_IfFloat(float value) => Assert.Equal(Math.Abs(value), value.Abs());
+    
+    [Theory]
+    [InlineData(0.0)]
+    [InlineData(1.1)]
+    [InlineData(-1.1)]
+    [InlineData(double.MaxValue)]
+    [InlineData(double.MinValue + 1)]
+    public void Abs_ShouldBehaveAsMathAbs_IfDouble(double value) => Assert.Equal(Math.Abs(value), value.Abs());
+    
+//    This is not working at a time due to some implicit conversion error along with decimal unavailable constants.
+//    [Theory]
+//    [InlineData(0.0)]
+//    [InlineData(1.1)]
+//    [InlineData(-1.1)]
+//    [InlineData(double.MaxValue)]
+//    [InlineData(double.MinValue + 1)]
+//    public void Abs_ShouldBehaveAsMathAbs_IfDecimal(decimal value) => Assert.Equal(Math.Abs(value), value.Abs());
+
+    #endregion
+    
     public class OrCoupleTest
     {
       [Theory]
