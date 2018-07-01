2f50bccb2549734630477f817dc40532bc00df7f
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-01 05:01:10 -0700

    Added tests for `SocketExtensions.IsConnected` method.

--- src/Core/Extensions/SocketExtensions.cs
+++ src/Core/Extensions/SocketExtensions.cs
@@ -2,8 +2,17 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents extension-methods for <see cref="Socket"/>.
+    /// </summary>
     public static class SocketExtensions
     {
+        /// <summary>
+        ///     Checks whether <code>self</code> is connected to client. 
+        /// </summary>
+        /// <remarks>This method uses some polling to do that</remarks>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns><code>true</code> if <paramref name="self"/> is connected, otherwise <code>false</code>.</returns>
         public static bool IsConnected(this Socket self)
         {
             try
---
+++ src/Tests/Extensions/SocketExtensionsTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Xunit;
+
+namespace Pocket.Common.Tests.Extensions
+{
+    public class SocketExtensionsTest
+    {
+        [Fact]
+        public void IsConnected_ShouldBeTrue_IfSocketIsConnected()
+        {
+            using (Start())
+            {
+                var client = Connect();
+            
+                Assert.True(client.Socket.IsConnected());
+            }
+        }
+        
+        [Fact]
+        public void IsConnected_ShouldBeTrue_IfSocketIsConnectedOnServer()
+        {
+            using (var server = Start())
+            {
+                Connect();
+                var serverClient = server.Accept();
+            
+                Assert.True(serverClient.IsConnected());
+            }
+        }
+        
+        [Fact]
+        public void IsConnected_ShouldBeFalse_IfSocketIsConnectedOnServerAndThenClosed()
+        {
+            using (var server = Start())
+            {
+                var client = Connect();
+                var serverClient = server.Accept();
+
+                client.Socket.Close();
+                
+                Assert.False(serverClient.IsConnected());
+            }
+        }
+        
+        [Fact]
+        public void IsConnected_ShouldBeFalse_IfSocketIsConnectedOnServerAndThenDisposed()
+        {
+            using (var server = Start())
+            {
+                var client = Connect();
+                var serverClient = server.Accept();
+
+                client.Disconnect();
+                
+                Assert.False(serverClient.IsConnected());
+            }
+        }
+        
+        [Fact]
+        public void IsConnected_ShouldThrow_IfSocketIsDisposed()
+        {
+            using (Start())
+            {
+                var client = Connect();
+                client.Socket.Close();
+            
+                Assert.Throws<ObjectDisposedException>(() => client.Socket.IsConnected());
+            }
+        }
+
+        #region Helpers
+
+        private class Server : IDisposable
+        {
+            public const string Ip = "127.0.0.1";
+            public const int Port = 4752;
+
+            private readonly TcpListener _listener;
+
+            public Server()
+            {
+                _listener = new TcpListener(IPAddress.Parse(Ip), Port);
+                _listener.Start();
+            }
+
+            public Socket Accept() => _listener.AcceptSocket();
+            
+            public void Dispose()
+            {
+                _listener.Stop();
+            }
+        }
+        
+        private class Client
+        {
+            private readonly TcpClient _client;
+
+            public Client()
+            {
+                _client = new TcpClient();
+                _client.Connect(IPAddress.Parse(Server.Ip), Server.Port);
+            }
+
+            public Socket Socket => _client.Client;
+
+            public void Disconnect() => _client.Dispose();
+        }
+        
+        private static Server Start() => new Server();
+        private static Client Connect() => new Client();
+
+        #endregion
+    }
+}
