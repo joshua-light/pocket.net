59bc174b2fa3a551c49e7b8b756321b206c85068
JoshuaLight <ashkariel.eter@gmail.com>
2018-07-29 05:43:21 -0700

    Added comments to `BoolExtensions`.

--- src/Core/Logic/BoolExtensions.cs
+++ src/Core/Logic/BoolExtensions.cs
@@ -1,11 +1,40 @@
 ﻿namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents extension-methods for <see cref="bool"/>.
+    /// </summary>
     public static class BoolExtensions
     {
+        /// <summary>
+        ///     Applies logical <code>&&</code> operator for both <paramref name="self"/> and <paramref name="other"/> values.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Second operand of logical operation.</param>
+        /// <returns>Result of <code>&&</code> operator aplied to <paramref name="self"/> and <paramref name="other"/>.</returns>
         public static bool And(this bool self, bool other) => self && other;
+        
+        /// <summary>
+        ///     Applies logical <code>||</code> operator for both <paramref name="self"/> and <paramref name="other"/> values.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Second operand of logical operation.</param>
+        /// <returns><paramref name="self"/> || <paramref name="other"/>.</returns>
         public static bool Or(this bool self, bool other) => self || other;
 
+        /// <summary>
+        ///     Applies logical implication operator for both <paramref name="self"/> and <paramref name="other"/> values.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Second operand of logical operation.</param>
+        /// <returns>If <paramref name="self"/> is <code>true</code>, then <paramref name="other"/>, otherwise <code>true</code>.</returns>
         public static bool Implication(this bool self, bool other) => !self || other;
+        
+        /// <summary>
+        ///     Applies logical equivalence operator for both <paramref name="self"/> and <paramref name="other"/> values.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">Second operand of logical operation.</param>
+        /// <returns>If <paramref name="self"/> is <code>true</code>, then <paramref name="other"/>, otherwise inverted <paramref name="other"/>.</returns>
         public static bool Equivalence(this bool self, bool other) => !self && !other || self && other;
     }
 }
