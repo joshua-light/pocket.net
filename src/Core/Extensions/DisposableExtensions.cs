94c78199accc61bdffc1a5992fe0945c247ea0c2
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-01 09:32:24 -0700

    Added `DisposableExtensions.Using` fluent method.

---
+++ src/Core/Extensions/DisposableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pocket.Common
+{
+    public static class DisposableExtensions
+    {
+        public static TOut Using<TIn, TOut>(this TIn self, Func<TIn, TOut> map) where TIn : IDisposable
+        {
+            map.EnsureNotNull();
+
+            return self
+                .As(map)
+                .Do(_ => self.Dispose());
+        }
+    }
+}
---
+++ src/Tests/Extensions/DisposableExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NSubstitute;
+using Xunit;
+
+namespace Pocket.Common.Tests.Extensions
+{
+    public class DisposableExtensionsTest
+    {
+        [Fact]
+        public void Using_ShouldCallDispose()
+        {
+            var disposable = Substitute.For<IDisposable>();
+            
+            disposable.Using(x => "1");
+            
+            disposable.Received(1).Dispose();
+        }
+        
+        [Fact]
+        public void Using_ShouldCallMapFuncOnDisposable()
+        {
+            var disposable = Substitute.For<IDisposable>();
+            var map = Substitute.For<Func<IDisposable, string>>();
+            
+            disposable.Using(map);
+            
+            map.Received(1).Invoke(disposable);
+        }
+        
+        [Fact]
+        public void Using_ShouldThrowArgumentNullException_IfMapIsNull() =>
+            Assert.Throws<ArgumentNullException>(() => Substitute.For<IDisposable>().Using<IDisposable, int>(null));
+    }
+}
