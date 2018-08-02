fa652a31646f59820e96d21efa5d245c37e7f00d
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-02 10:33:05 -0700

    Added comments to `DisposableExtensions.Using`.

--- src/Core/Extensions/DisposableExtensions.cs
+++ src/Core/Extensions/DisposableExtensions.cs
@@ -2,8 +2,20 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents extension-methods for <see cref="IDisposable"/>.
+    /// </summary>
     public static class DisposableExtensions
     {
+        /// <summary>
+        ///     Uses <see cref="IDisposable"/> object to produce other object through <paramref name="map"/> function.
+        ///     Then disposes.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="map">Function that maps <see cref="IDisposable"/> object to object of type <typeparamref name="TOut"/>.</param>
+        /// <typeparam name="TIn">Type of <see cref="IDisposable"/> object.</typeparam>
+        /// <typeparam name="TOut">Type of result object.</typeparam>
+        /// <returns></returns>
         public static TOut Using<TIn, TOut>(this TIn self, Func<TIn, TOut> map) where TIn : IDisposable
         {
             map.EnsureNotNull();
