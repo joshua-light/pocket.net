2170fba2c0f5e814ee3d81d2c2bc62ebd9bb93b1
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-03 09:50:34 -0700

    Added `ManualResetEventGate`.

---
+++ src/Core/Threading/ManualResetEventGate.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+
+namespace Pocket.Common
+{
+    public struct ManualResetEventGate
+    {
+        private readonly ManualResetEvent _event;
+        
+        public ManualResetEventGate(bool opened = true)
+        {
+            _event = new ManualResetEvent(opened);
+        }
+
+        public bool IsOpened => _event.WaitOne(0);
+        
+        public void Open() => _event.Set();
+        public void Close() => _event.Reset();
+        public void WaitForOpen() => _event.WaitOne();
+    }
+}
