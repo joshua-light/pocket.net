724adbc8bb5e0a681af128d93ab6a9088e57067f
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-04 07:43:22 -0700

    Added `Map` class that inherits `Dictionary`.

---
+++ src/Core/System/Map.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace Pocket.Common
+{
+    /// <inheritdoc cref="Dictionary{TKey,TValue}"/>.
+    public class Map<TKey, TValue> : Dictionary<TKey, TValue> { }
+}
