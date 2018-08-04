67ec3f90b64c58852dd764ce349310c3cf69afb3
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-04 07:42:01 -0700

    Added comments to `ThreadGate`.

--- src/Core/Threading/ThreadGate.cs
+++ src/Core/Threading/ThreadGate.cs
@@ -2,20 +2,42 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents gate that can be used for threads synchronization in same way as <see cref="ManualResetEvent"/>.
+    /// </summary>
     public struct ThreadGate
     {
         private readonly ManualResetEvent _event;
         
+        /// <summary>
+        ///     Initializes new instance of <see cref="ThreadGate"/>.
+        /// </summary>
+        /// <param name="opened">Initial gate state.</param>
         public ThreadGate(bool opened = true)
         {
             _event = new ManualResetEvent(opened);
         }
 
+        /// <summary>
+        ///     Determines whether gate is in opened state or not.
+        /// </summary>
         public bool IsOpened => _event.WaitOne(0);
         
+        /// <summary>
+        ///     Opens gate.
+        /// </summary>
         public void Open() => _event.Set();
+        
+        /// <summary>
+        ///     Closes gate.
+        /// </summary>
         public void Close() => _event.Reset();
         
-        public bool WaitForOpen(int ms = -1) => _event.WaitOne(ms);
+        /// <summary>
+        ///     Blocks current thread for specified amount of time (or infinite) until gate is became opened.
+        /// </summary>
+        /// <param name="timeoutMs">How much time thread should be blocked in milliseconds.</param>
+        /// <returns><code>true</code> if gate was opened while thread was blocked, otherwise — <code>false</code>.</returns>
+        public bool WaitForOpen(int timeoutMs = -1) => _event.WaitOne(timeoutMs);
     }
 }
