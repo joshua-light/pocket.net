aa0195a0497408a633535475a7a58503a33630d2
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-04 23:43:25 +0300

    Added comments to `OneTime`.

--- src/Core/Monads/OneTime.cs
+++ src/Core/Monads/OneTime.cs
@@ -2,17 +2,29 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents value that has some initial state for one read and then resets to default.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class OneTime<T> where T : IEquatable<T>
     {
         private readonly T _default;
         private T _current;
 
+        /// <summary>
+        ///     Initializes instance of <see cref="OneTime{T}"/>.
+        /// </summary>
+        /// <param name="initial">Initial value that will be represented by <see cref="Value"/> for one read.</param>
+        /// <param name="default">Default value.</param>
         public OneTime(T initial, T @default)
         {
             _default = @default;
             _current = initial;
         }
 
+        /// <summary>
+        ///     For first time returns initial value, then always returns default value.
+        /// </summary>
         public T Value
         {
             get
