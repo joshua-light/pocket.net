d108ae4af83c4643163539517890fc7c3bebc7c1
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-04 23:03:19 +0300

    Added comments to `Cold`.

--- src/Core/Monads/Cold.cs
+++ src/Core/Monads/Cold.cs
@@ -2,10 +2,18 @@
 
 namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents value that can be initialized without constructor but only once.
+    /// </summary>
+    /// <typeparam name="T">Type of value.</typeparam>
     public class Cold<T>
     {
         private T _value;
 
+        /// <summary>
+        ///     Internal value that is initialized through <see cref="Freeze"/>.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">There is no value because <see cref="Freeze"/> wasn't called.</exception>
         public T Value
         {
             get
@@ -17,14 +25,24 @@ namespace Pocket.Common
             }
         }
         
+        /// <summary>
+        ///     Determines whether value was initialized through <see cref="Freeze"/>.
+        /// </summary>
         public bool IsFrozen { get; private set; }
         
+        /// <summary>
+        ///     Initializes internal state with some concrete value.
+        /// </summary>
+        /// <param name="value">Value that will be an internal state of <see cref="Cold{T}"/>.</param>
+        /// <returns>Instance of <code>this</code>.</returns>
+        /// <exception cref="InvalidOperationException">Value is already initialized.</exception>
         public Cold<T> Freeze(T value)
         {
             if (IsFrozen)
                 throw new InvalidOperationException("Cannot freeze twice.");
             
             _value = value;
+            
             IsFrozen = true;
             return this;
         }
