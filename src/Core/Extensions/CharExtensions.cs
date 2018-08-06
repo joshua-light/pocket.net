4e91f0729cda38323ee75a8ffe53a9a034211a31
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-06 20:22:43 +0300

    Added `CharExtensions.IsDigit` extension-method.

---
+++ src/Core/Extensions/CharExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Pocket.Common
+{
+    /// <summary>
+    ///     Represents extension-methods for <see cref="char"/>.
+    /// </summary>
+    public static class CharExtensions
+    {
+        /// <summary>
+        ///     Checks whether current instance is digit character.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns><code>true</code> if <paramref name="self"/> is digit, otherwise — <code>false</code>.</returns>
+        public static bool IsDigit(this char self) => char.IsDigit(self);
+    }
+}
---
+++ src/Tests/Extensions/CharExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Pocket.Common.Tests.Extensions
+{
+    public class CharExtensionsTest
+    {
+        [Theory]
+        [InlineData('0')]
+        [InlineData('1')]
+        [InlineData('2')]
+        [InlineData('3')]
+        [InlineData('4')]
+        [InlineData('5')]
+        [InlineData('6')]
+        [InlineData('7')]
+        [InlineData('8')]
+        [InlineData('9')]
+        public void IsDigit_ShouldBeTrue_IfCharIsDigit(char ch) =>
+            ch.IsDigit().ShouldBeTrue();
+        
+        [Theory]
+        [InlineData('`')]
+        [InlineData('a')]
+        [InlineData('b')]
+        [InlineData('c')]
+        [InlineData('\n')]
+        [InlineData('\r')]
+        [InlineData(')')]
+        public void IsDigit_ShouldBeFalse_IfCharIsNotDigit(char ch) =>
+            ch.IsDigit().ShouldBeFalse();
+    }
+}
