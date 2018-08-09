86c1984a6f90962ac3e11ec143c87fadb76f3087
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-10 01:16:37 +0300

    Added `TimeSpanExtensions` along with `Milliseconds` method.

---
+++ src/Core/Extensions/TimeSpanExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Pocket.Common
+{
+    /// <summary>
+    ///     Represents extension-methods for <see cref="TimeSpan"/> (or for converting to it).
+    /// </summary>
+    public static class TimeSpanExtensions
+    {
+        /// <summary>
+        ///     Represents number as milliseconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> milliseconds.</returns>
+        public static TimeSpan Milliseconds(this int self) => TimeSpan.FromMilliseconds(self);
+        
+        /// <summary>
+        ///     Represents number as milliseconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> milliseconds.</returns>
+        public static TimeSpan Milliseconds(this long self) => TimeSpan.FromMilliseconds(self);
+        
+        /// <summary>
+        ///     Represents number as milliseconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> milliseconds.</returns>
+        public static TimeSpan Milliseconds(this float self) => TimeSpan.FromMilliseconds(self);
+        
+        /// <summary>
+        ///     Represents number as milliseconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> milliseconds.</returns>
+        public static TimeSpan Milliseconds(this double self) => TimeSpan.FromMilliseconds(self);
+    }
+}
