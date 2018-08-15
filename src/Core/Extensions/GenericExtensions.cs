71ca3368db094856055ad3f42d6132fc0e1fc576
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-15 20:32:28 +0300

    Added `GenericExtensions` with `Or` method.

---
+++ src/Core/Extensions/GenericExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Pocket.Common
+{
+    public static class GenericExtensions
+    {
+        public static T Or<T>(this T self, T @default) where T : class =>
+            self ?? @default;
+    }
+}
---
+++ src/Tests/Extensions/GenericExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Pocket.Common.Tests.Extensions
+{
+    public class GenericExtensionsTest
+    {
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("Hello")]
+        [InlineData("World")]
+        public void Or_ShouldReturnSelf_IfItIsNotNull(string value) =>
+            value.Or("1").ShouldBe(value);
+
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("Hello")]
+        [InlineData("World")]
+        public void Or_ShouldReturnDefaultValue_IfSelfIsNull(string def) =>
+            ((string) null).Or(def).ShouldBe(def);
+    }
+}
