715467a9a87b51f7ae5edd685ddba57c1db7f3d9
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-16 10:03:49 +0300

    Added comments to `GenericExtensions`.

--- src/Core/Extensions/GenericExtensions.cs
+++ src/Core/Extensions/GenericExtensions.cs
@@ -1,7 +1,17 @@
 ﻿namespace Pocket.Common
 {
+    /// <summary>
+    ///     Represents generic extension-methods for objects of all types.
+    /// </summary>
     public static class GenericExtensions
     {
+        /// <summary>
+        ///     Represents either <paramref name="self"/> or <paramref name="default"/> if the first one is <code>null</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="default">Default value that will be used instead of <paramref name="self"/> if it's <code>null</code>.</param>
+        /// <typeparam name="T">Type of values.</typeparam>
+        /// <returns>Either <paramref name="self"/> or <paramref name="default"/>.</returns>
         public static T Or<T>(this T self, T @default) where T : class =>
             self ?? @default;
     }
