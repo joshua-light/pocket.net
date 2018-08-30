10281a2d7ec3a34efd2eed5eff71afea28c19364
JoshuaLight <ashkariel.eter@gmail.com>
2018-08-31 00:51:07 +0300

    Added `StringExtensions.IsNullOrEmpty`.
    Closes #12.

---
+++ src/Core/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Pocket.Common
+{
+    /// <summary>
+    ///     Represents extension-methods for <see cref="string"/>.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        ///     Checks whether <paramref name="self"/> is <code>null</code> or empty string.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns><code>true</code> if <paramref name="self"/> is <code>null</code> or empty string, otherwise — <code>false</code>.</returns>
+        public static bool IsNullOrEmpty(this string self) => 
+            string.IsNullOrEmpty(self);
+    }
+}
---
+++ src/Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,23 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Pocket.Common.Tests.Extensions
+{
+    public class StringExtensionsTest
+    {
+        [Theory]
+        [InlineData("1")]
+        [InlineData("2")]
+        [InlineData("Hello")]
+        public void IsNullOrEmpty_ShouldBeFalse_IfStringIsNotNullOrEmpty(string value) =>
+            value.IsNullOrEmpty().ShouldBeFalse();
+        
+        [Fact]
+        public void IsNullOrEmpty_ShouldBeTrue_IfStringIsNull() =>
+            ((string) null).IsNullOrEmpty().ShouldBeTrue();
+        
+        [Fact]
+        public void IsNullOrEmpty_ShouldBeTrue_IfStringIsEmpty() =>
+            "".IsNullOrEmpty().ShouldBeTrue();
+    }
+}
