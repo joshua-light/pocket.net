4b35d17921fdb75b7743fe63c20e4169da87f234
JoshuaLight <ashkariel.eter@gmail.com>
2018-09-01 21:05:30 +0300

    Added comments to `CharExtensions`.

--- src/Core/Extensions/CharExtensions.cs
+++ src/Core/Extensions/CharExtensions.cs
@@ -12,10 +12,25 @@
         /// <returns><code>true</code> if <paramref name="self"/> is digit, otherwise — <code>false</code>.</returns>
         public static bool IsDigit(this char self) => char.IsDigit(self);
 
+        /// <summary>
+        ///     Checks whether current instance is letter character.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns><code>true</code> if <paramref name="self"/> is letter, otherwise — <code>false</code>.</returns>
         public static bool IsLetter(this char self) => char.IsLetter(self);
 
+        /// <summary>
+        ///     Checks whether current instance is letter or digit character.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns><code>true</code> if <paramref name="self"/> is  letter or digit, otherwise — <code>false</code>.</returns>
         public static bool IsLetterOrDigit(this char self) => char.IsLetterOrDigit(self);
 
+        /// <summary>
+        ///     Checks whether current instance is whitespace character.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns><code>true</code> if <paramref name="self"/> is whitespace, otherwise — <code>false</code>.</returns>
         public static bool IsWhiteSpace(this char self) => char.IsWhiteSpace(self);
     }
 }
