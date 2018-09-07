8e2160f0376eb74047696114a8ed75619969c746
JoshuaLight <ashkariel.eter@gmail.com>
2018-09-07 23:50:49 +0300

    Added `Nullable<T>.OrDefault` extension-method.
    Closes #15.

--- src/Core/Extensions/GenericExtensions.cs
+++ src/Core/Extensions/GenericExtensions.cs
@@ -14,5 +14,14 @@
         /// <returns>Either <paramref name="self"/> or <paramref name="default"/>.</returns>
         public static T Or<T>(this T self, T @default) where T : class =>
             self ?? @default;
+
+        /// <summary>
+        ///     Represents <paramref name="self"/> as its value or as default of <typeparamref name="T"/> if <paramref name="self"/> is <code>null</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <typeparam name="T">Type of values.</typeparam>
+        /// <returns>Either <paramref name="self"/> inner value or default of <typeparamref name="T"/>.</returns>
+        public static T OrDefault<T>(this T? self) where T : struct =>
+            self.GetValueOrDefault();
     }
 }
--- src/Tests/Extensions/GenericExtensionsTest.cs
+++ src/Tests/Extensions/GenericExtensionsTest.cs
@@ -20,5 +20,16 @@ namespace Pocket.Common.Tests.Extensions
         [InlineData("World")]
         public void Or_ShouldReturnDefaultValue_IfSelfIsNull(string def) =>
             ((string) null).Or(def).ShouldBe(def);
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public void OrDefault_ShouldReturnSelf_IfItIsNotNull(int value) =>
+            new int?(value).OrDefault().ShouldBe(value);
+
+
+        [Fact]
+        public void OrDefault_ShouldReturnDefaultValue_IfSelfIsNull() =>
+            ((int?) null).OrDefault().ShouldBe(0);
     }
 }
