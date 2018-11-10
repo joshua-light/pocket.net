c92069eec618215234359686c734afacca5c5d1a
JoshuaLight <ashkariel.eter@gmail.com>
2018-11-10 21:50:44 +0200

    Added XML-comments to `MethodInfoExtensions`.

--- src/Core/Extensions/Reflection/MethodInfoExtensions.cs
+++ src/Core/Extensions/Reflection/MethodInfoExtensions.cs
@@ -2,8 +2,16 @@ using System.Reflection;
 
 namespace Pocket.Common
 {
+  /// <summary>
+  ///     Represents extension-methods for <see cref="MethodInfo"/>.
+  /// </summary>
   public static class MethodInfoExtensions
   {
+    /// <summary>
+    ///   Gets arguments of specified method.
+    /// </summary>
+    /// <param name="self"><code>this</code> object.</param>
+    /// <returns>An array of <see cref="ParameterInfo"/> that represent arguments of method.</returns>
     public static ParameterInfo[] Arguments(this MethodInfo self) =>
       self.GetParameters();
   }
