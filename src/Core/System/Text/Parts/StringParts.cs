f1bb4cd0c1daec5545cc0b13c4fd940fb4b7029a
JoshuaLight <ashkariel.eter@gmail.com>
2018-11-25 17:51:35 +0200

    Added laziness to `StringBuilder` in `StringParts`.

--- src/Core/System/Text/Parts/StringParts.cs
+++ src/Core/System/Text/Parts/StringParts.cs
@@ -4,7 +4,7 @@ namespace Pocket.Common
 {
     public struct StringParts
     {
-        private readonly StringBuilder _string;
+        private StringBuilder _string;
         
         internal StringParts(string a, string b, string c, string d, string e)
         {
@@ -18,12 +18,16 @@ namespace Pocket.Common
 
         public StringParts With(string part)
         {
+            _string = _string ?? new StringBuilder();
             _string.Append(part);
 
             return this;
         }
 
         public static implicit operator string(StringParts self) =>
-            self._string.ToString();
+            self._string?.ToString() ?? "";
+
+        public override string ToString() =>
+            this;
     }
 }
