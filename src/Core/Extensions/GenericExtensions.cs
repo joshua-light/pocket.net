177ab7d20477b820bbce62f0de8baf1eb7629ac7
JoshuaLight <ashkariel.eter@gmail.com>
2018-11-29 15:09:12 +0200

    Added `GenericExtensions.Or` for `T?`.

--- src/Core/Extensions/GenericExtensions.cs
+++ src/Core/Extensions/GenericExtensions.cs
@@ -16,6 +16,16 @@
             self ?? @default;
 
         /// <summary>
+        ///     Represents either <paramref name="self"/> or <paramref name="default"/> if the first one is <code>null</code>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="default">Default value that will be used instead of <paramref name="self"/> if it's <code>null</code>.</param>
+        /// <typeparam name="T">Type of values.</typeparam>
+        /// <returns>Either <paramref name="self"/> or <paramref name="default"/>.</returns>
+        public static T Or<T>(this T? self, T @default) where T : struct =>
+            self ?? @default;
+        
+        /// <summary>
         ///     Represents <paramref name="self"/> as its value or as default of <typeparamref name="T"/> if <paramref name="self"/> is <code>null</code>.
         /// </summary>
         /// <param name="self"><code>this</code> object.</param>
