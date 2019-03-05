9d608a292ac80d5ec3a5d735d8d7938155b45f11
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-05 13:59:32 +0200

    Added `includingParents` to `MemberInfoExtensions.Has`.

--- src/Core/Extensions/Reflection/MemberInfoExtensions.cs
+++ src/Core/Extensions/Reflection/MemberInfoExtensions.cs
@@ -5,8 +5,8 @@ namespace Pocket.Common
 {
     public static class MemberInfoExtensions
     {
-        public static bool Has<T>(this MemberInfo self) where T : Attribute =>
-            self.Attribute<T>() != null;
+        public static bool Has<T>(this MemberInfo self, bool includingParents = true) where T : Attribute =>
+            self.IsDefined(typeof(T), includingParents);
 
         public static T Attribute<T>(this MemberInfo self) where T : Attribute =>
             self.GetCustomAttribute<T>();
