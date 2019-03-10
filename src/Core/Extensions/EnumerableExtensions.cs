d1bd5f3dfca185569da0ca4c6b8da318575a43d1
Joshua Light <ashkariel.eter@gmail.com>
2019-03-10 14:48:10 -0700

    Added comment to `StringExtensions.Separated`.

--- src/Core/Extensions/EnumerableExtensions.cs
+++ src/Core/Extensions/EnumerableExtensions.cs
@@ -249,8 +249,22 @@ namespace Pocket.Common
             return default;
         }
 
+        /// <summary>
+        ///     Concatenates the members of a sequence, using specified string as separator.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="with">String that will be inserted between items of <paramref name="self"/>.</param>
+        /// <typeparam name="T">Type of elements in sequence.</typeparam>
+        /// <returns>Items of <paramref name="self"/> converted to string and separated with <paramref name="with"/>.</returns>
         public static string Separated<T>(this IEnumerable<T> self, string with) =>
             string.Join(with, self);
+        
+        /// <summary>
+        ///     Concatenates the members of a sequence, using specified string as separator.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="with">String that will be inserted between items of <paramref name="self"/>.</param>
+        /// <returns>Items of <paramref name="self"/> converted to string and separated with <paramref name="with"/>.</returns>
         public static string Separated(this IEnumerable<string> self, string with) =>
             string.Join(with, self);
     }
