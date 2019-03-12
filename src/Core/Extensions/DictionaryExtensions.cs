2d612e4805faf3fee766ab5ec84f08d1a8bfa750
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-12 11:54:28 +0200

    Added `One` with `TValue orNew` to `DictionaryExtensions`.

--- src/Core/Extensions/DictionaryExtensions.cs
+++ src/Core/Extensions/DictionaryExtensions.cs
@@ -24,12 +24,24 @@ namespace Pocket.Common
         /// </summary>
         /// <param name="self"><code>this</code> object.</param>
         /// <param name="key">Key of element to get.</param>
-        /// <param name="or">Function that creates new value.</param>
+        /// <param name="orNew">New value.</param>
         /// <typeparam name="TKey">Type of keys in dictionary.</typeparam>
         /// <typeparam name="TValue">Type of values in dictionary.</typeparam>
         /// <returns>Element or newly created value with specified key.</returns>
-        public static TValue One<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, Func<TValue> or) =>
-            self.TryGetValue(key, out var result) ? result : self[key] = or();
+        public static TValue One<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, TValue orNew) =>
+            self.TryGetValue(key, out var result) ? result : self[key] = orNew;
+        
+        /// <summary>
+        ///     Gets element by specified key or sets new value, if one doesn't exist.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="key">Key of element to get.</param>
+        /// <param name="orNew">Function that creates new value.</param>
+        /// <typeparam name="TKey">Type of keys in dictionary.</typeparam>
+        /// <typeparam name="TValue">Type of values in dictionary.</typeparam>
+        /// <returns>Element or newly created value with specified key.</returns>
+        public static TValue One<TKey, TValue>(this IDictionary<TKey, TValue> self, TKey key, Func<TValue> orNew) =>
+            self.TryGetValue(key, out var result) ? result : self[key] = orNew();
 
         /// <summary>
         ///     Gets element by specified key or throws exception with more verbose message than indexer's one.
