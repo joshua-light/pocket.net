032ed2824ae31b1463d6a9f2a7d92ba3e16ec916
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-14 13:54:36 +0200

    Added `Instances` type that allows instantiations through reflection.

---
+++ src/Core/System/Instances.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Pocket.Common
+{
+  public static class Instances
+  {
+    public struct AssemblyExpression<T>
+    {
+      private readonly Predicate<Type> _predicate;
+
+      public AssemblyExpression(Predicate<Type> predicate) =>
+        _predicate = predicate;
+
+      public List<T> InThisAssembly() => In(Assembly.GetCallingAssembly());
+      public List<T> In(Assembly assembly)
+      {
+        var all = new List<T>();
+        
+        foreach (var type in assembly.GetTypes())
+          if (_predicate(type))
+            all.Add((T) type.Instantiate());
+
+        return all;
+      }
+    }
+    
+    public static AssemblyExpression<T> Of<T>() where T : class =>
+      new AssemblyExpression<T>(x => typeof(T).IsInterface && x.Implements<T>() || typeof(T).IsClass && x.Extends<T>());
+    
+    public static AssemblyExpression<T> ThatExtend<T>() where T : class =>
+      new AssemblyExpression<T>(x => typeof(T).IsInterface && x.Implements<T>());
+    
+    public static AssemblyExpression<T> ThatImplement<T>() where T : class =>
+      new AssemblyExpression<T>(x => typeof(T).IsClass && x.Extends<T>());
+  }
+}
---
+++ src/Tests/System/InstancesTest.cs
@@ -0,0 +1,62 @@
+using System.Linq;
+using Shouldly;
+using Xunit;
+
+namespace Pocket.Common.Tests.System
+{
+  public class InstancesTest
+  {
+    [Fact]
+    public void InstancesOf_IInterface_ShouldBeAllImplementations() =>
+      Instances.Of<IInterface>().InThisAssembly().Select(x => x.GetType()).ShouldBe(new[]
+      {
+        typeof(Implementation1),
+        typeof(Implementation2),
+        typeof(Implementation3),
+        typeof(Implementation4),
+        typeof(Implementation5),
+        typeof(ChildAndImplementation1),
+        typeof(ChildAndImplementation2),
+        typeof(ChildAndImplementation3),
+        typeof(ChildAndImplementation4),
+        typeof(ChildAndImplementation5),
+      });
+    
+    [Fact]
+    public void InstancesOf_Class_ShouldBeAllChilds() =>
+      Instances.Of<Class>().InThisAssembly().Select(x => x.GetType()).ShouldBe(new[]
+      {
+        typeof(Child1),
+        typeof(Child2),
+        typeof(Child3),
+        typeof(Child4),
+        typeof(Child5),
+        typeof(ChildAndImplementation1),
+        typeof(ChildAndImplementation2),
+        typeof(ChildAndImplementation3),
+        typeof(ChildAndImplementation4),
+        typeof(ChildAndImplementation5),
+      });
+    
+    public interface IInterface { }
+    public abstract class Class { }
+    
+    public class Implementation1 : IInterface { }
+    public class Implementation2 : IInterface { }
+    public class Implementation3 : IInterface{ }
+    public class Implementation4 : IInterface{ }
+    public class Implementation5 : IInterface{ }
+    
+    public class Child1 : Class  { }
+    public class Child2 : Class  { }
+    public class Child3 : Class  { }
+    public class Child4 : Class  { }
+    public class Child5 : Class  { }
+    
+    public class ChildAndImplementation1 : Class, IInterface { }
+    public class ChildAndImplementation2 : Class, IInterface { }
+    public class ChildAndImplementation3 : Class, IInterface { }
+    public class ChildAndImplementation4 : Class, IInterface { }
+    public class ChildAndImplementation5 : Class, IInterface { }
+  }
+}
