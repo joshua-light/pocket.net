6efdcc435385676510a0e434552ebabc0a3f467a
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-18 20:21:33 +0200

    Added overloads of `Change` and `Changed` with other types to `Concurrently`.

--- src/Core/System/Concurrently.cs
+++ src/Core/System/Concurrently.cs
@@ -6,8 +6,28 @@ namespace Pocket.Common
   {
     public static bool Changed(ref int self, int from, int to) =>
       Change(ref self, from, to) == from;
+    public static bool Changed(ref long self, long from, long to) =>
+      Change(ref self, from, to) == from;
+    public static bool Changed(ref float self, float from, float to) =>
+      Change(ref self, from, to) == from;
+    public static bool Changed(ref double self, double from, double to) =>
+      Change(ref self, from, to) == from;
+    public static bool Changed(ref object self, object from, object to) =>
+      Change(ref self, from, to) == from;
+    public static bool Changed<T>(ref T self, T from, T to) where T : class =>
+      Change(ref self, from, to) == from;
     
     public static int Change(ref int self, int from, int to) =>
+      Interlocked.CompareExchange(ref self, to, from);
+    public static long Change(ref long self, long from, long to) =>
+      Interlocked.CompareExchange(ref self, to, from);
+    public static float Change(ref float self, float from, float to) =>
+      Interlocked.CompareExchange(ref self, to, from);
+    public static double Change(ref double self, double from, double to) =>
+      Interlocked.CompareExchange(ref self, to, from);
+    public static object Change(ref object self, object from, object to) =>
+      Interlocked.CompareExchange(ref self, to, from);
+    public static T Change<T>(ref T self, T from, T to) where T : class =>
       Interlocked.CompareExchange(ref self, to, from);
   }
 }
