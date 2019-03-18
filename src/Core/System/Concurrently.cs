821417b016d99e39388c3ac257590605531cc5be
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-18 18:03:36 +0200

    Added `Changed` method to `Concurrently`.

--- src/Core/System/Concurrently.cs
+++ src/Core/System/Concurrently.cs
@@ -4,6 +4,9 @@ namespace Pocket.Common
 {
   public static class Concurrently
   {
+    public static bool Changed(ref int self, int from, int to) =>
+      Change(ref self, from, to) == from;
+    
     public static int Change(ref int self, int from, int to) =>
       Interlocked.CompareExchange(ref self, to, from);
   }
