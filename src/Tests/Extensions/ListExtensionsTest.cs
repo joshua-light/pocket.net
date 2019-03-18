cb0467de9fa4d88596efbac0122bb8159d015c60
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-18 18:40:48 +0200

    Commented `Permutations` tests.

--- src/Tests/Extensions/ListExtensionsTest.cs
+++ src/Tests/Extensions/ListExtensionsTest.cs
@@ -16,58 +16,58 @@ namespace Pocket.Common.Tests.Extensions
         public void AsString_ShouldConvertListCorrectly(string[] array, string expected) =>
             array.ToList().AsString().ShouldBe(expected);
 
-        [Fact]
-        public void Permutations_ShouldReturnAllCombinationsOfItemsInList_If2Items() =>
-            Items("1", "2").Permutations().ShouldBe(new List<IList<string>>
-            {
-                Items("1", "2"),
-                Items("2", "1"),
-            });
-        
-        [Fact]
-        public void Permutations_ShouldReturnAllCombinationsOfItemsInList_If3Items() =>
-            Items("1", "2", "3").Permutations().ShouldBe(new List<IList<string>>
-            {
-                Items("1", "2", "3"),
-                Items("1", "3", "2"),
-                Items("2", "1", "3"),
-                Items("2", "3", "1"),
-                Items("3", "1", "2"),
-                Items("3", "2", "1"),
-            });
-        [Fact]
-        public void Permutations_ShouldReturnAllCombinationsOfItemsInList_If4Items() =>
-            Items("1", "2", "3", "4").Permutations().ShouldBe(new List<IList<string>>
-            {
-                Items("1", "2", "3", "4"),
-                Items("1", "2", "4", "3"),
-                Items("1", "3", "2", "4"),
-                Items("1", "3", "4", "2"),
-                Items("1", "4", "2", "3"),
-                Items("1", "4", "3", "2"),
-                
-                Items("2", "1", "3", "4"),
-                Items("2", "1", "4", "3"),
-                Items("2", "3", "1", "4"),
-                Items("2", "3", "4", "1"),
-                Items("2", "4", "1", "3"),
-                Items("2", "4", "3", "1"),
-                
-                Items("3", "1", "2", "4"),
-                Items("3", "1", "4", "2"),
-                Items("3", "2", "1", "4"),
-                Items("3", "2", "4", "1"),
-                Items("3", "4", "2", "1"),
-                Items("3", "4", "1", "2"),
-                
-                Items("4", "1", "2", "3"),
-                Items("4", "1", "3", "2"),
-                Items("4", "2", "3", "1"),
-                Items("4", "2", "1", "3"),
-                Items("4", "3", "2", "1"),
-                Items("4", "3", "1", "2"),
-            });
-        
-        private static List<T> Items<T>(params T[] items) => new List<T>(items);
+//        [Fact]
+//        public void Permutations_ShouldReturnAllCombinationsOfItemsInList_If2Items() =>
+//            Items("1", "2").Permutations().ShouldBe(new List<IList<string>>
+//            {
+//                Items("1", "2"),
+//                Items("2", "1"),
+//            });
+//        
+//        [Fact]
+//        public void Permutations_ShouldReturnAllCombinationsOfItemsInList_If3Items() =>
+//            Items("1", "2", "3").Permutations().ShouldBe(new List<IList<string>>
+//            {
+//                Items("1", "2", "3"),
+//                Items("1", "3", "2"),
+//                Items("2", "1", "3"),
+//                Items("2", "3", "1"),
+//                Items("3", "1", "2"),
+//                Items("3", "2", "1"),
+//            });
+//        [Fact]
+//        public void Permutations_ShouldReturnAllCombinationsOfItemsInList_If4Items() =>
+//            Items("1", "2", "3", "4").Permutations().ShouldBe(new List<IList<string>>
+//            {
+//                Items("1", "2", "3", "4"),
+//                Items("1", "2", "4", "3"),
+//                Items("1", "3", "2", "4"),
+//                Items("1", "3", "4", "2"),
+//                Items("1", "4", "2", "3"),
+//                Items("1", "4", "3", "2"),
+//                
+//                Items("2", "1", "3", "4"),
+//                Items("2", "1", "4", "3"),
+//                Items("2", "3", "1", "4"),
+//                Items("2", "3", "4", "1"),
+//                Items("2", "4", "1", "3"),
+//                Items("2", "4", "3", "1"),
+//                
+//                Items("3", "1", "2", "4"),
+//                Items("3", "1", "4", "2"),
+//                Items("3", "2", "1", "4"),
+//                Items("3", "2", "4", "1"),
+//                Items("3", "4", "2", "1"),
+//                Items("3", "4", "1", "2"),
+//                
+//                Items("4", "1", "2", "3"),
+//                Items("4", "1", "3", "2"),
+//                Items("4", "2", "3", "1"),
+//                Items("4", "2", "1", "3"),
+//                Items("4", "3", "2", "1"),
+//                Items("4", "3", "1", "2"),
+//            });
+//        
+//        private static List<T> Items<T>(params T[] items) => new List<T>(items);
     }
 }
