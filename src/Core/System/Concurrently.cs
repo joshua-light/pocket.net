20dfedf3fd115df19d728d43a62060f9f166c195
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-18 18:03:05 +0200

    Renamed `Concurrent` to `Concurrently`.

--- src/Core/System/Concurrently.cs
+++ src/Core/System/Concurrently.cs
@@ -2,7 +2,7 @@ using System.Threading;
 
 namespace Pocket.Common
 {
-  public static class Concurrent
+  public static class Concurrently
   {
     public static int Change(ref int self, int from, int to) =>
       Interlocked.CompareExchange(ref self, to, from);
