6621bcc89f11b22dc5a4ffc87348f1b910c05ca7
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-19 20:15:45 +0200

    Added `whenFirstDayIs` to `DateTimeExtensions.StartOfWeek`.

--- src/Core/Extensions/DateTimeExtensions.cs
+++ src/Core/Extensions/DateTimeExtensions.cs
@@ -4,11 +4,12 @@ namespace Pocket.Common
 {
     public static class DateTimeExtensions
     {
-        public static DateTime StartOfWeek(this DateTime time)
+        public static DateTime StartOfWeek(this DateTime time) => time.StartOfWeek(whenFirstDayIs: DayOfWeek.Monday);
+        public static DateTime StartOfWeek(this DateTime time, DayOfWeek whenFirstDayIs)
         {
             const int daysInWeek = 7;
             
-            var days =  daysInWeek - time.DayOfWeek.DaysTo(DayOfWeek.Monday);
+            var days =  daysInWeek - time.DayOfWeek.DaysTo(whenFirstDayIs);
             if (days == daysInWeek)
                 return time.Date;
       
