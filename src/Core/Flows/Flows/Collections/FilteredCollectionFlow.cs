eff60adcb76e221a5758127e7f6b2cb34b13d000
Joshua Light <ashkariel.eter@gmail.com>
2019-03-23 03:29:01 -0700

    Optimized `FilteredCollectionFlow` a bit.

--- src/Core/Flows/Flows/Collections/FilteredCollectionFlow.cs
+++ src/Core/Flows/Flows/Collections/FilteredCollectionFlow.cs
@@ -6,19 +6,20 @@ namespace Pocket.Common.Flows
 {
     internal sealed class FilteredCollectionFlow<T> : ICollectionFlow<T>
     {
-        private readonly ICollectionFlow<T> _source;
-        private readonly Func<T, bool> _predicate;
+        private readonly IList<T> _source;
 
         public FilteredCollectionFlow(ICollectionFlow<T> source, Func<T, bool> predicate)
         {
-            _source = source;
-            _predicate = predicate;
+            _source = new List<T>(source.Current.Where(predicate));
 
-            Added = _source.Added.Where(predicate);
-            Removed = _source.Removed.Where(predicate);
+            Added = source.Added.Where(predicate);
+            Removed = source.Removed.Where(predicate);
+
+            Added.OnNext(x => _source.Add(x));
+            Removed.OnNext(x => _source.Remove(x));
         }
 
-        public IEnumerable<T> Current => _source.Current.Where(_predicate);
+        public IEnumerable<T> Current => _source;
         
         public IFlow<T> Added { get; }
         public IFlow<T> Removed { get; }
