10909ea2993188f998ab114efff243802b1c90fe
Joshua Light <ashkariel.eter@gmail.com>
2019-03-26 16:17:19 -0700

    Added `Of` to `Disposable`.

--- src/Core/System/Disposable/Disposable.cs
+++ src/Core/System/Disposable/Disposable.cs
@@ -10,5 +10,11 @@ namespace Pocket.Common
         }
         
         public static readonly IDisposable Fake = new FakeDisposable();
+
+        public static IDisposable Of(Action begin, Action end) =>
+            new CompactDisposable(begin, end);
+
+        public static IDisposable Of<T>(T x, Action<T> begin, Action<T> end) =>
+            new CompactDisposable(() => begin(x), () => end(x));
     }
 }
