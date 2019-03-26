b1fe368212258488e7684f0f56bfaf8a73613faf
Joshua Light <ashkariel.eter@gmail.com>
2019-03-26 16:04:43 -0700

    Cleaned up `DirectorInfoExtensions`.

--- src/Core/Extensions/DirectoryInfoExtensions.cs
+++ src/Core/Extensions/DirectoryInfoExtensions.cs
@@ -11,23 +11,20 @@ namespace Pocket.Common
     {
       private readonly DirectoryInfo _root;
 
-      public CopyExpression(DirectoryInfo root)
-      {
+      public CopyExpression(DirectoryInfo root) =>
         _root = root;
-      }
 
+      public CopyFilesExpression Files =>
+        new CopyFilesExpression(_root);
+      
       public CopyDirectoriesExpression Where(Predicate<FileInfo> file) =>
         new CopyDirectoriesExpression(_root, file);
+      
       public CopyDirectoriesExpression Where(Predicate<DirectoryInfo> directory) =>
         new CopyDirectoriesExpression(_root, null, directory);
-
-      public CopyFilesExpression Files => new CopyFilesExpression(_root);
       
-      public void To(string path, string mask = "*.*")
-      {
-        new CopyDirectoriesExpression(_root)
-          .To(path, mask);
-      }
+      public void To(string path, string mask = "*.*") =>
+        new CopyDirectoriesExpression(_root).To(path, mask);
     }
     
     public struct CopyFilesExpression
