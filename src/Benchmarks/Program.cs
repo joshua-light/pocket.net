48947f8840e8ff8e160891a5f805a79425ac9db6
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-28 22:33:54 +0200

    Added new `Benchmarks` project.

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Pocket.Benchmarks
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Benchmark.OfAssembly().Execute();
+        }
+
+        [Sample(times: 10000)]
+        public class TestSample
+        {
+            [Run]
+            public int SumRun() => 2 + 2;
+
+            [Run]
+            public int MultiplicationRun() => 2 * 2;
+        }
+    }
+}
