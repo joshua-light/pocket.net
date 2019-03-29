30441bbed046ee62b9e817fc05a767a4e0f489ba
JoshuaLight <ashkariel.eter@gmail.com>
2019-03-29 18:55:38 +0200

    Added `Add` to `Atomically`.

--- src/Core/Threading/Atomically.cs
+++ src/Core/Threading/Atomically.cs
@@ -34,5 +34,10 @@ namespace Pocket.Common
       Interlocked.Increment(ref self);
     public static long Increment(ref long self) =>
       Interlocked.Increment(ref self);
+
+    public static void Add(ref int self, int value) =>
+      Interlocked.Add(ref self, value);
+    public static void Add(ref long self, long value) =>
+      Interlocked.Add(ref self, value);
   }
 }
