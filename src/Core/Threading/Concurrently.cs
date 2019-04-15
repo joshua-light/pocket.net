1472c48b95192654c57a112b1ba77529bdecc7f8
JoshuaLight <ashkariel.eter@gmail.com>
2019-04-15 20:28:13 +0300

    Added `Concurrently` class with `Add` methods that simplify locking.

---
+++ src/Core/Threading/Concurrently.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Pocket.Common
+{
+    public static class Concurrently
+    {
+        public static void Add<T>(T item, IList<T> to) =>
+            Add(item, to, with: to);
+
+        public static void Add<T>(T item, IList<T> to, object with)
+        {
+            lock (with) to.Add(item);
+        }
+    }
+}
