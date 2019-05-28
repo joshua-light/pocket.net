30e3d1160e8b7e987c44f0b34a18ba7754e38acc
JoshuaLight <ashkariel.eter@gmail.com>
2019-05-28 11:00:37 +0300

    Added `CollectionExtensionsTest`.

--- src/Core/Extensions/Collections/CollectionExtensions.cs
+++ src/Core/Extensions/Collections/CollectionExtensions.cs
@@ -1,10 +1,32 @@
+using System;
 using System.Collections.Generic;
+using static Pocket.Common.Guard;
 
 namespace Pocket.Common
 {
-  public static class CollectionExtensions
-  {
-    public static void AddRange<T>(this ICollection<T> self, IEnumerable<T> other) =>
-      other.ForEach(self.Add);
-  }
+    /// <summary>
+    ///     Represents extension-methods for <see cref="ICollection{T}"/>.
+    /// </summary>
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        ///     Adds elements of the specified collection <paramref name="other"/> to the end of <paramref name="self"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <param name="other">The collection whose elements should be added to the end of <paramref name="self"/>.</param>
+        /// <typeparam name="T">Type of elements in <see cref="ICollection{T}"/>.</typeparam>
+        /// <exception cref="ArgumentNullException"><paramref name="self"/> or <paramref name="other"/> is <code>null</code>.</exception>
+        public static void AddRange<T>(this ICollection<T> self, IEnumerable<T> other)
+        {
+            Ensure(that: self).NotNull();
+            Ensure(that: other).NotNull();
+            
+            if (other is List<T> list)
+                foreach (var x in list)
+                    self.Add(x);
+            else
+                foreach (var x in other)
+                    self.Add(x);
+        }
+    }
 }
---
+++ src/Tests/Extensions/Collections/CollectionExtensionsTest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using Shouldly;
+using Xunit;
+using static Xunit.Assert;
+
+namespace Pocket.Common.Tests.Extensions.Collections
+{
+    public class CollectionExtensionsTest
+    {
+        [Fact] public void AddRange_ShouldThrow_IfSelfIsNull() =>
+            Throws<ArgumentNullException>(() => Null().AddRange(Of(1, 2, 3)));
+        [Fact] public void AddRange_ShouldThrow_IfOtherIsNull() =>
+            Throws<ArgumentNullException>(() => New().AddRange(Null()));
+
+        [Fact] public void AddRange_ShouldAddAllElementsToCollection() =>
+            Add(Of(1, 2, 3), to: New()).ShouldBe(new[] { 1, 2, 3 });
+
+        private static ICollection<int> Null() => null;
+        private static ICollection<int> New() => new List<int>();
+        private static IEnumerable<int> Of(params int[] args) => args;
+
+        private static ICollection<int> Add(IEnumerable<int> items, ICollection<int> to) =>
+            to.Do(x => x.AddRange(items));
+    }
+}
