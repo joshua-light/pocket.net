3f5d0977cb568a9a686edcd3eddc0d1d16de8bea
JoshuaLight <ashkariel.eter@gmail.com>
2019-06-04 13:10:03 +0300

    Added some extensions to `TimeSpanExtensions`.

--- src/Core/Extensions/TimeSpanExtensions.cs
+++ src/Core/Extensions/TimeSpanExtensions.cs
@@ -34,5 +34,25 @@ namespace Pocket.Common
         /// <param name="self"><code>this</code> object.</param>
         /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> milliseconds.</returns>
         public static TimeSpan Ms(this double self) => TimeSpan.FromMilliseconds(self);
+        
+        public static TimeSpan Seconds(this int self) => TimeSpan.FromSeconds(self);
+        public static TimeSpan Seconds(this long self) => TimeSpan.FromSeconds(self);
+        public static TimeSpan Seconds(this float self) => TimeSpan.FromSeconds(self);
+        public static TimeSpan Seconds(this double self) => TimeSpan.FromSeconds(self);
+        
+        public static TimeSpan Minutes(this int self) => TimeSpan.FromMinutes(self);
+        public static TimeSpan Minutes(this long self) => TimeSpan.FromMinutes(self);
+        public static TimeSpan Minutes(this float self) => TimeSpan.FromMinutes(self);
+        public static TimeSpan Minutes(this double self) => TimeSpan.FromMinutes(self);
+        
+        public static TimeSpan Hours(this int self) => TimeSpan.FromHours(self);
+        public static TimeSpan Hours(this long self) => TimeSpan.FromHours(self);
+        public static TimeSpan Hours(this float self) => TimeSpan.FromHours(self);
+        public static TimeSpan Hours(this double self) => TimeSpan.FromHours(self);
+        
+        public static TimeSpan Hour(this int self) => TimeSpan.FromHours(self);
+        public static TimeSpan Hour(this long self) => TimeSpan.FromHours(self);
+        public static TimeSpan Hour(this float self) => TimeSpan.FromHours(self);
+        public static TimeSpan Hour(this double self) => TimeSpan.FromHours(self);
     }
 }
