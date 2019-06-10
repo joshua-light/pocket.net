bd8d30c54350771dd0ebe286bf63a50e9fffa1ee
JoshuaLight <ashkariel.eter@gmail.com>
2019-06-10 11:59:28 +0300

    Added comments to `TimeSpanExtensions`.

--- src/Core/Extensions/TimeSpanExtensions.cs
+++ src/Core/Extensions/TimeSpanExtensions.cs
@@ -35,24 +35,116 @@ namespace Pocket.Common
         /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> milliseconds.</returns>
         public static TimeSpan Ms(this double self) => TimeSpan.FromMilliseconds(self);
         
+        /// <summary>
+        ///     Represents number as seconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> seconds.</returns>
         public static TimeSpan Seconds(this int self) => TimeSpan.FromSeconds(self);
+        
+        /// <summary>
+        ///     Represents number as seconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> seconds.</returns>
         public static TimeSpan Seconds(this long self) => TimeSpan.FromSeconds(self);
+        
+        /// <summary>
+        ///     Represents number as seconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> seconds.</returns>
         public static TimeSpan Seconds(this float self) => TimeSpan.FromSeconds(self);
+        
+        /// <summary>
+        ///     Represents number as seconds in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> seconds.</returns>
         public static TimeSpan Seconds(this double self) => TimeSpan.FromSeconds(self);
         
+        /// <summary>
+        ///     Represents number as minutes in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> minutes.</returns>
         public static TimeSpan Minutes(this int self) => TimeSpan.FromMinutes(self);
+        
+        /// <summary>
+        ///     Represents number as minutes in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> minutes.</returns>
         public static TimeSpan Minutes(this long self) => TimeSpan.FromMinutes(self);
+        
+        /// <summary>
+        ///     Represents number as minutes in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> minutes.</returns>
         public static TimeSpan Minutes(this float self) => TimeSpan.FromMinutes(self);
+        
+        /// <summary>
+        ///     Represents number as minutes in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> minutes.</returns>
         public static TimeSpan Minutes(this double self) => TimeSpan.FromMinutes(self);
         
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hours(this int self) => TimeSpan.FromHours(self);
+        
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hours(this long self) => TimeSpan.FromHours(self);
+        
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hours(this float self) => TimeSpan.FromHours(self);
+        
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hours(this double self) => TimeSpan.FromHours(self);
         
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hour(this int self) => TimeSpan.FromHours(self);
+        
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hour(this long self) => TimeSpan.FromHours(self);
+        
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hour(this float self) => TimeSpan.FromHours(self);
+        
+        /// <summary>
+        ///     Represents number as hours in <see cref="TimeSpan"/>.
+        /// </summary>
+        /// <param name="self"><code>this</code> object.</param>
+        /// <returns>Instance of <see cref="TimeSpan"/> with size of <paramref name="self"/> hours.</returns>
         public static TimeSpan Hour(this double self) => TimeSpan.FromHours(self);
     }
 }
