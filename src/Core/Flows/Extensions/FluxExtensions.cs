f20efcf07aa019ad6c68983f6703a9c70432df06
JoshuaLight <ashkariel.eter@gmail.com>
2019-09-21 15:18:05 +0300

    Added some `FluxExtensions`.

--- src/Core/Flows/Extensions/FluxExtensions.cs
+++ src/Core/Flows/Extensions/FluxExtensions.cs
@@ -13,8 +13,28 @@ namespace Pocket.Common.Flows
             self.Change(by: +1);
         public static void Decrement(this IFlux<int> self) =>
             self.Change(by: -1);
-
         public static void Change(this IFlux<int> self, int by) =>
+            self.Pulse(self.Current + by);
+        
+        public static void Increment(this IFlux<long> self) =>
+            self.Change(by: +1L);
+        public static void Decrement(this IFlux<long> self) =>
+            self.Change(by: -1L);
+        public static void Change(this IFlux<long> self, long by) =>
+            self.Pulse(self.Current + by);
+        
+        public static void Increment(this IFlux<float> self) =>
+            self.Change(by: +1.0f);
+        public static void Decrement(this IFlux<float> self) =>
+            self.Change(by: -1.0f);
+        public static void Change(this IFlux<float> self, float by) =>
+            self.Pulse(self.Current + by);
+        
+        public static void Increment(this IFlux<double> self) =>
+            self.Change(by: +1.0);
+        public static void Decrement(this IFlux<double> self) =>
+            self.Change(by: -1.0);
+        public static void Change(this IFlux<double> self, double by) =>
             self.Pulse(self.Current + by);
     }
 }
