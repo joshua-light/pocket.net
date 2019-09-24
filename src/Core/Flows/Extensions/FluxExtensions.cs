bad64a8511e463816cc8cee9eed737894d7b3e54
JoshuaLight <ashkariel.eter@gmail.com>
2019-09-25 00:04:48 +0300

    Added `Increase` and `Decrease` to `FluxExtensions`.

--- src/Core/Flows/Extensions/FluxExtensions.cs
+++ src/Core/Flows/Extensions/FluxExtensions.cs
@@ -13,6 +13,10 @@ namespace Pocket.Common.Flows
             self.Change(by: +1);
         public static void Decrement(this IFlux<int> self) =>
             self.Change(by: -1);
+        public static void Increase(this IFlux<int> self, int by) =>
+            self.Change(by);
+        public static void Decrease(this IFlux<int> self, int by) =>
+            self.Change(-by);
         public static void Change(this IFlux<int> self, int by) =>
             self.Pulse(self.Current + by);
         
@@ -20,6 +24,10 @@ namespace Pocket.Common.Flows
             self.Change(by: +1L);
         public static void Decrement(this IFlux<long> self) =>
             self.Change(by: -1L);
+        public static void Increase(this IFlux<long> self, long by) =>
+            self.Change(by);
+        public static void Decrease(this IFlux<long> self, long by) =>
+            self.Change(-by);
         public static void Change(this IFlux<long> self, long by) =>
             self.Pulse(self.Current + by);
         
@@ -27,6 +35,10 @@ namespace Pocket.Common.Flows
             self.Change(by: +1.0f);
         public static void Decrement(this IFlux<float> self) =>
             self.Change(by: -1.0f);
+        public static void Increase(this IFlux<float> self, long by) =>
+            self.Change(by);
+        public static void Decrease(this IFlux<float> self, long by) =>
+            self.Change(-by);
         public static void Change(this IFlux<float> self, float by) =>
             self.Pulse(self.Current + by);
         
@@ -34,6 +46,10 @@ namespace Pocket.Common.Flows
             self.Change(by: +1.0);
         public static void Decrement(this IFlux<double> self) =>
             self.Change(by: -1.0);
+        public static void Increase(this IFlux<double> self, long by) =>
+            self.Change(by);
+        public static void Decrease(this IFlux<double> self, long by) =>
+            self.Change(-by);
         public static void Change(this IFlux<double> self, double by) =>
             self.Pulse(self.Current + by);
     }
