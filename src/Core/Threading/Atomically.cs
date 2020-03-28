0a2e098ddcdbd08c1c8519ccde0cfe679655ac97
JoshuaLight <ashkariel.eter@gmail.com>
2020-03-28 20:07:54 +0200

    Added `Change` to `Atomically`.

--- src/Core/Threading/Atomically.cs
+++ src/Core/Threading/Atomically.cs
@@ -2,42 +2,72 @@ using System.Threading;
 
 namespace Pocket.Common
 {
-  public static class Atomically
-  {
-    public static bool Changed(ref int self, int from, int to) =>
-      Change(ref self, from, to) == from;
-    public static bool Changed(ref long self, long from, long to) =>
-      Change(ref self, from, to) == from;
-    public static bool Changed(ref float self, float from, float to) =>
-      Change(ref self, from, to) == from;
-    public static bool Changed(ref double self, double from, double to) =>
-      Change(ref self, from, to) == from;
-    public static bool Changed(ref object self, object from, object to) =>
-      Change(ref self, from, to) == from;
-    public static bool Changed<T>(ref T self, T from, T to) where T : class =>
-      Change(ref self, from, to) == from;
-    
-    public static int Change(ref int self, int from, int to) =>
-      Interlocked.CompareExchange(ref self, to, from);
-    public static long Change(ref long self, long from, long to) =>
-      Interlocked.CompareExchange(ref self, to, from);
-    public static float Change(ref float self, float from, float to) =>
-      Interlocked.CompareExchange(ref self, to, from);
-    public static double Change(ref double self, double from, double to) =>
-      Interlocked.CompareExchange(ref self, to, from);
-    public static object Change(ref object self, object from, object to) =>
-      Interlocked.CompareExchange(ref self, to, from);
-    public static T Change<T>(ref T self, T from, T to) where T : class =>
-      Interlocked.CompareExchange(ref self, to, from);
+    public static class Atomically
+    {
+        public static bool Changed(ref int self, int from, int to) =>
+            Change(ref self, from, to) == from;
 
-    public static int Increment(ref int self) =>
-      Interlocked.Increment(ref self);
-    public static long Increment(ref long self) =>
-      Interlocked.Increment(ref self);
+        public static bool Changed(ref long self, long from, long to) =>
+            Change(ref self, from, to) == from;
 
-    public static void Add(ref int self, int value) =>
-      Interlocked.Add(ref self, value);
-    public static void Add(ref long self, long value) =>
-      Interlocked.Add(ref self, value);
-  }
+        public static bool Changed(ref float self, float from, float to) =>
+            Change(ref self, from, to) == from;
+
+        public static bool Changed(ref double self, double from, double to) =>
+            Change(ref self, from, to) == from;
+
+        public static bool Changed(ref object self, object from, object to) =>
+            Change(ref self, from, to) == from;
+
+        public static bool Changed<T>(ref T self, T from, T to) where T : class =>
+            Change(ref self, from, to) == from;
+
+        public static int Change(ref int self, int from, int to) =>
+            Interlocked.CompareExchange(ref self, to, from);
+
+        public static long Change(ref long self, long from, long to) =>
+            Interlocked.CompareExchange(ref self, to, from);
+
+        public static float Change(ref float self, float from, float to) =>
+            Interlocked.CompareExchange(ref self, to, from);
+
+        public static double Change(ref double self, double from, double to) =>
+            Interlocked.CompareExchange(ref self, to, from);
+
+        public static object Change(ref object self, object from, object to) =>
+            Interlocked.CompareExchange(ref self, to, from);
+
+        public static T Change<T>(ref T self, T from, T to) where T : class =>
+            Interlocked.CompareExchange(ref self, to, from);
+
+        public static int Change(ref int self, int to) =>
+            Interlocked.Exchange(ref self, to);
+
+        public static long Change(ref long self, long to) =>
+            Interlocked.Exchange(ref self, to);
+
+        public static float Change(ref float self, float to) =>
+            Interlocked.Exchange(ref self, to);
+
+        public static double Change(ref double self, double to) =>
+            Interlocked.Exchange(ref self, to);
+
+        public static object Change(ref object self, object to) =>
+            Interlocked.Exchange(ref self, to);
+
+        public static T Change<T>(ref T self, T to) where T : class =>
+            Interlocked.Exchange(ref self, to);
+
+        public static int Increment(ref int self) =>
+            Interlocked.Increment(ref self);
+
+        public static long Increment(ref long self) =>
+            Interlocked.Increment(ref self);
+
+        public static void Add(ref int self, int value) =>
+            Interlocked.Add(ref self, value);
+
+        public static void Add(ref long self, long value) =>
+            Interlocked.Add(ref self, value);
+    }
 }
